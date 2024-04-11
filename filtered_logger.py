b33356c95450a356f511367352d81971da299493
Emmanuel Masambu <125499034+EMANU02004@users.noreply.github.com>
2024-04-11 14:12:55 +0000

    filtered_logger.py

--- filtered_logger.py
+++ filtered_logger.py
@@ -11,3 +11,13 @@ import logging
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """ Replacing """
+    for entry in fields:
+        message = re.sub(rf"{entry}=(.*?)\{separator}",
+                         f'{entry}={redaction}{separator}', message)
+    return message
+
+
