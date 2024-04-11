e4516f9adcc9df33928f8bc363ef342c438dbfa8
Emmanuel Masambu <125499034+EMANU02004@users.noreply.github.com>
2024-04-11 14:14:19 +0000

    filtered_logger.py

--- filtered_logger.py
+++ filtered_logger.py
@@ -21,3 +21,30 @@ def filter_datum(fields: List[str], redaction: str, message: str,
     return message
 
 
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """ Replacing """
+    for entry in fields:
+        message = re.sub(rf"{entry}=(.*?)\{separator}",
+                         f'{entry}={redaction}{separator}', message)
+    return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """ Init """
+        self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Filter values in incoming logs """
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
+
